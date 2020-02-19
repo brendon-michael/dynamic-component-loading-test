867a1d56ede1f733d0c06598de037309184774bc
Brendon McGay <brendon.mcgay@gmail.com>
2020-02-19 13:53:57 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as settings from './componentsToLoad.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testing-connect';
+  componentsToLoad: string;
+  ngOnInit() {
+    console.log(settings.componentsToLoad[0]);
+    this.componentsToLoad = settings.componentsToLoad[0];
+    // this.componentsToLoad = '<app-testing> </app-testing>';
+  }
 }
